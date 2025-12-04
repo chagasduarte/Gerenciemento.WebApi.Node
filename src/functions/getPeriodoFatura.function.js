faa4ff345aad2aad0386b314e9a044f697843379
Chagas Duarte <francisco.c@luizalabs.com>
2025-12-04 11:11:17 -0300

    voltando

--- src/functions/getPeriodoFatura.function.js
+++ src/functions/getPeriodoFatura.function.js
@@ -17,7 +17,7 @@ export function getPeriodoFatura(diaFatura, mes, ano) {
 
   if (diaFatura === 1) {
     inicio = `${anoAnterior}-${mmAnterior}-01`;
-    fim = `${ano}-${mmAnterior}-${ultimoDiaMesAnterior}`;
+    fim = `${anoAnterior}-${mmAnterior}-${ultimoDiaMesAnterior}`;
   } else {
     inicio = `${anoAnterior}-${mmAnterior}-${String(diaFatura).padStart(2, '0')}`;
     fim = `${ano}-${mmAtual}-${String(diaFatura - 1).padStart(2, '0')}`;
